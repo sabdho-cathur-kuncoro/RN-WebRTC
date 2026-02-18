1adb4d014cfa1e05b82ee392a47c4ffeb85df9bd
RDS-MOBILE <rds-mobile@RDS-MOBILEs-MacBook-Pro.local>
2026-02-18 17:35:20 +0700

    remove console log

--- components/AudioSink/index.tsx
+++ components/AudioSink/index.tsx
@@ -1,13 +1,8 @@
 import { useMediasoupContext } from "@/contexts/MediaSoupContext";
-import { useEffect } from "react";
 import { RTCView } from "react-native-webrtc";
 
 export function AudioSink() {
   const { remoteStream } = useMediasoupContext();
-
-  useEffect(() => {
-    console.log("🔊 COMPONENT AudioSink stream", remoteStream?.id);
-  }, [remoteStream]);
 
   return (
     <RTCView
